fa8ede3fb02c3ba663f8d65e398df8b109be5624
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-08-14 15:48:00 +0100

    empty database created with the tables

--- templates/database.sql
+++ templates/database.sql
@@ -1,7 +1,11 @@
-DROP TABLE IF EXISTS member;
-DROP TABLE IF EXISTS books;
-DROP TABLE IF EXISTS copies_of_books;
 DROP TABLE IF EXISTS checkout_history;
+DROP TABLE IF EXISTS copies_of_books;
+
+
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS member;
+
+
 
 CREATE TABLE IF NOT EXISTS member (
     id              serial primary key,
@@ -14,19 +18,23 @@ CREATE TABLE IF NOT EXISTS member (
 CREATE TABLE IF NOT EXISTS books (
     id              serial primary key,
     title           varchar(16) not null,
-    auther     varchar(128),
+    author     varchar(128) not null,
     genre       varchar(128) not null,
     release_date    int not null
 );
+
 CREATE TABLE IF NOT EXISTS copies_of_books (
     id              serial primary key,
     book_id           int REFERENCES books(id) not null
 );
-CREATE TABLE IF NOT EXISTS house (
+
+CREATE TABLE IF NOT EXISTS checkout_history (
     id       serial primary key,
-    user_id   int REFERENCES user(id) not null,
+    user_id   int REFERENCES member(id) not null,
     copy_id   int REFERENCES copies_of_books(id) not null,
     return_date  date not null,
     check_in   date not null,
     check_out  date not null
+
 );
+

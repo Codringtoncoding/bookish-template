de0f1ad8caec87770ceb8c210e2c63ec3ceeac18
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-08-17 10:34:58 +0100

    populating database

--- templates/database.sql
+++ templates/database.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS member (
     name           varchar(16) not null,
     phone_number      varchar(128) not null,
     email       varchar(128) not null,
-    adress       varchar(1000) not null
+    address       varchar(1000) not null
 );
 
 CREATE TABLE IF NOT EXISTS books (
@@ -38,3 +38,34 @@ CREATE TABLE IF NOT EXISTS checkout_history (
 
 );
 
+INSERT INTO member (id, name, phone_number, email, address)
+VALUES
+('book_junkie', '97560355675', 'bookie@bookz.biz', 'Sunny av.56, Mordor'),
+('drunkie', '97560399775', 'elf@magicland.biz', 'Sunny av.56, Mordor'),
+('book_woman', '97568885675', 'boo@bookz.biz', 'Moon av.56, Mordor'),
+('traitor', '9756535675', 'monster@hotmail.biz', 'Bland street, Mordor'),
+('sodalobster', '97566531675', 'lobster@bookz.biz', 'Fishy av.99 Mordor')
+
+INSERT INTO books (id, title, author, genre, release_date)
+VALUES
+('Harry Potter and the Sorcerers Stone','J.K.Rowling', 'Fantasy/Family', 2001),
+('The Lord of the Rings: The Fellowship of the Ring', 'J. R. R. Tolkien','Adventure fiction', 2001),
+('How to Win Friends and Influence People', 'Dale Carnegie', 'Self-help ',1936),
+('To Kill a Mockingbird', 'Harper Lee', 'Southern Gothic' ,1960),
+('The Great Gatsby', 'F. Scott Fitzgerald','Tragedy', 1925 );
+
+INSERT INTO copies_of_books (id, book_id)
+VALUES 
+(1),
+(1),
+(1),
+(3),
+(3),
+(3),
+(5);
+
+INSERT INTO checkout_history (user_id, copy_id, return_date, check_in, check_out)
+VALUES
+(2, 3,'2020-10-20', '2020-08-20', '2020-09-30'),
+(1, 5,'2020-09-23', '2020-09-23', '2020-10-05'),
+(4, 4,'2020-09-19', '2020-08-30', '2020-08-06');
